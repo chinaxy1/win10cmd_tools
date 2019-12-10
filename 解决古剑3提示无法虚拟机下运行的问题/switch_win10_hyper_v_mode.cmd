8e9dadc50a65c2adfef6be52d151c1178523315c
ViMColD <chinaxy1@163.com>
2019-12-11 02:38:40 +0800

    feat: 新增解决古剑3启动提示无法在虚拟机下运行的问题CMD

---
+++ 解决古剑3提示无法虚拟机下运行的问题/switch_win10_hyper_v_mode.cmd
@@ -0,0 +1,63 @@
+@echo off
+@title switch_win10_hyper_v_mode
+
+:: ------------------------------
+:: 解决古剑奇谭3在win10专业版打开时
+:: 出现不能运行在虚拟机环境下的问题
+:: （注：需要以管理员身份运行该CMD）
+:: ------------------------------
+
+rem 是否展示当前BCDEDIT配置，默认否
+rem call:showCurrentBcd
+
+:main
+echo 请选择： 1. 关闭Hyper-V；  2：恢复BCD启动项
+echo;
+
+set /p input=您的选项为：
+
+if %input% equ 1 (
+    call:closeHyperV
+) else if %input% equ 2 (
+    call:resetBcdConfig
+) else (
+    echo 没有这个选项！
+    echo;
+    goto:main
+)
+
+echo;
+echo 请重启电脑以生效操作！
+rem call:reboot
+echo;
+
+pause
+goto:eof
+
+:: ------------------------------
+:: method_begin
+:: ------------------------------
+
+:showCurrentBcd
+echo 当前本机的BCDEDIT首选项配置为：
+echo;
+bcdedit /v
+echo;
+goto:eof
+
+:closeHyperV
+bcdedit /set hypervisorlaunchtype Off
+goto:eof
+
+
+:resetBcdConfig
+bcdedit /set hypervisorlaunchtype Auto
+goto:eof
+
+:reboot
+shutdown -r -t 0
+goto:eof
+
+:: ------------------------------
+:: method_end
+:: ------------------------------
